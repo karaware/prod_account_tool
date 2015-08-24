c9b3b8155062f03a98ff8f7322f084fba6ca7770
k.hirata <k.hirata@qloog.com>
2015-08-24 12:22:54 +0900

    平田がテス

---
+++ check.js
@@ -0,0 +1,31 @@
+/* エンターキーでの'submit'を禁止する */
+$(function(){
+    $("input"). keydown(function(e) {
+        if ((e.which && e.which === 13) || (e.keyCode && e.keyCode === 13)) {
+	    return false;
+        } else {
+            return true;
+        }
+    });
+});
+
+
+/* validationEngineをform_1に適用 */
+jQuery(function(){
+    jQuery("#form_1").validationEngine();
+});
+
+
+/* パスワード生成ツール */
+function getPassword() {
+    var len = 16;
+    var seed0 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var seed = '';
+    seed =  seed0;
+    var pwd = '';
+    while (len--) {
+	pwd += seed0[Math.floor(Math.random() * seed.length)];
+    }
+    $('#result').val(pwd);
+}
+
